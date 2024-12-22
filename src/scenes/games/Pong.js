48f9d8ad9c71e416e0b8e6d7bd7359c38d7c96cb
Daniel Alves <dalima.dev@gmail.com>
2024-12-22 03:58:14 -0300

    add middle dotted line

--- src/scenes/games/Pong.js
+++ src/scenes/games/Pong.js
@@ -1,21 +1,40 @@
 import { Scene } from "phaser";
 
 const INITIAL_VELOCITY = 300;
+const INITIAL_LINE_POS = 30;
+const LINE_GAP = 5;
+const LINE_HEIGHT = 20;
 
 export class Pong extends Scene {
+  centerX;
+  centerY;
+
   constructor() {
     super("Pong");
   }
 
   init() {
-    // this.cameras.main.setBackgroundColor("#000000");
+    this.centerX = this.game.config.width / 2;
+    this.centerY = this.game.config.height / 2;
+
+    this.cameras.main.setBackgroundColor("#000000");
+
+    const graphics = this.add.graphics();
+    graphics.lineStyle(2, 0xffffff);
+
+    graphics.beginPath();
+    for (let i = 0; i < 20; i++) {
+      const positionY = i * INITIAL_LINE_POS + LINE_GAP;
+
+      graphics.moveTo(this.centerX, positionY);
+      graphics.lineTo(this.centerX, positionY + LINE_HEIGHT);
+    }
+    graphics.closePath();
+    graphics.strokePath();
   }
 
   create() {
-    const centerX = this.game.config.width / 2;
-    const centerY = this.game.config.height / 2;
-
-    const ball = this.add.rectangle(centerX, centerY, 5, 5, 0xffffff);
+    const ball = this.add.rectangle(this.centerX, this.centerY, 5, 5, 0xffffff);
     this.physics.add.existing(ball);
 
     ball.body
