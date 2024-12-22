82bacb627c1797c027cd499999fbb1ba4152eef8
Daniel Alves <dalima.dev@gmail.com>
2024-12-22 17:27:39 -0300

    implements players with collision with ball pixel

--- src/scenes/games/Pong.js
+++ src/scenes/games/Pong.js
@@ -4,11 +4,13 @@ const INITIAL_VELOCITY = 300;
 const INITIAL_LINE_POS = 30;
 const LINE_GAP = 5;
 const LINE_HEIGHT = 20;
+const OBJECTS_COLOR = 0xffffff;
+
+const PLAYER_WIDTH = 5;
+const PLAYER_HEIGHT = 50;
+const PLAYER_BOUND_DISTANCE = 30;
 
 export class Pong extends Scene {
-  centerX;
-  centerY;
-
   constructor() {
     super("Pong");
   }
@@ -20,7 +22,7 @@ export class Pong extends Scene {
     this.cameras.main.setBackgroundColor("#000000");
 
     const graphics = this.add.graphics();
-    graphics.lineStyle(2, 0xffffff);
+    graphics.lineStyle(2, OBJECTS_COLOR);
 
     graphics.beginPath();
     for (let i = 0; i < 20; i++) {
@@ -34,13 +36,44 @@ export class Pong extends Scene {
   }
 
   create() {
-    const ball = this.add.rectangle(this.centerX, this.centerY, 5, 5, 0xffffff);
-    this.physics.add.existing(ball);
+    this.ball = this.add.rectangle(
+      this.centerX,
+      this.centerY,
+      5,
+      5,
+      OBJECTS_COLOR
+    );
+    this.physics.add.existing(this.ball);
 
-    ball.body
+    this.firstPlayer = this.add.rectangle(
+      PLAYER_BOUND_DISTANCE,
+      this.centerY,
+      PLAYER_WIDTH,
+      PLAYER_HEIGHT,
+      OBJECTS_COLOR
+    );
+
+    this.secondPlayer = this.add.rectangle(
+      this.game.config.width - PLAYER_BOUND_DISTANCE,
+      this.centerY,
+      PLAYER_WIDTH,
+      PLAYER_HEIGHT,
+      OBJECTS_COLOR
+    );
+
+    this.physics.add.existing(this.firstPlayer);
+    this.physics.add.existing(this.secondPlayer);
+
+    this.physics.add.collider(this.ball, this.firstPlayer);
+    this.physics.add.collider(this.ball, this.secondPlayer);
+
+    this.firstPlayer.body.pushable = false;
+    this.secondPlayer.body.pushable = false;
+
+    this.ball.body
       .setVelocity(INITIAL_VELOCITY, INITIAL_VELOCITY)
       .setBounce(1, 1)
-      .setCollideWorldBounds(true);
+      .setCollideWorldBounds();
   }
 
   update() {}
